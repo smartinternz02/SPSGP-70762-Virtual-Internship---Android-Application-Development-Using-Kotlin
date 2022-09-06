261d6eedb9ba0b8722035482e301036677924d35
shubham-sapkal <sapkal.shubham2002@gmail.com>
2022-09-06 20:16:24 +0530

    init commit

---
+++ app/src/main/java/com/shubhamsapkal/bussinessnearme/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shubhamsapkal.bussinessnearme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
